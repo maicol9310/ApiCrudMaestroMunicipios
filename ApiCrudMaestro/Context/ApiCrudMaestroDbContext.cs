90061fa50c43f78878065fb9bc352764c8023182
Jan Michael Sanchez Sierra <maicol_931028@hotmail.com>
2021-11-24 11:21:38 -0500

    Add model and context

---
+++ ApiCrudMaestro/Context/ApiCrudMaestroDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiCrudMaestro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCrudMaestro.Context
+{
+    public class ApiCrudMaestroDbContext: DbContext
+    {
+        public ApiCrudMaestroDbContext(DbContextOptions<ApiCrudMaestroDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Municipios> Municipios { get; set; }
+    }
+}
---
+++ ApiCrudMaestro/Models/Municipios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace ApiCrudMaestro.Models
+{
+    public class Municipios
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ConsecutivoInterno { get; set; }
+        public string Nombre { get; set; }
+        public string Departamento { get; set; }
+        public string CodigoDane { get; set; }
+        public bool Distrito { get; set; }
+    }
+}
